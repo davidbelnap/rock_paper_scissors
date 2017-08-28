db5b8a6e23896e5db5fda4cf37e1dcf653e931c5
David Belnap <david.belnap6@gmail.com>
2017-08-28 15:19:05 -0600

    initial commit, text and images added, no jquery added

---
+++ main.js
@@ -0,0 +1,21 @@
+// var userChoice = prompt("Do you choose rock, paper, or scissors?");
+// var computerChoice = Math.random();
+//     if (computerChoice = ( 0 .. 0.33 )) {
+//         computerChoice = "rock";
+//     } else if (computerChoice = (0.34 .. 0.66)) {
+//         computerChoice = "paper";
+//     } else {
+//         computerChoice = "scissors";
+//     }
+//     console.log(computerChoice);
+
+// 	onclick newVar = Math.random() * 3
+// if newVar.round == 0
+// 	computerChoice = "rock";
+// else if newVar == 1
+// 	computerChoice = "paper";
+// else
+// 	computerChoice = "scissors";
+
+onClick.getElementById(rock)
+	debugger
